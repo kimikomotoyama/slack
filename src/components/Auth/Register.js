1d1257d83137a14ca77e5bdafedd6dde853f3c9e
Kimiko Motoyama <meowlingual@gmail.com>
2019-02-05 00:42:33 +0900

    handle submit on register user with firebase - user authentication is not complete

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Grid, Form, Segment, Button, Header, Message, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import firebase from "../../firebase";
 
 class Register extends React.Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    }
+  }
 
   render() {
+    const { username, email, password, passwordConfirmation } = this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -13,38 +23,46 @@ class Register extends React.Component {
             <Icon name="puzzle piece" color="orange" />
             Register for DevChat
           </Header>
-          <Form size="large">
+          <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
               <Form.Input
-                fluid name="username"
+                fluid
+                name="username"
                 icon="user"
                 iconPosition="left"
                 placeholder="Username"
                 onChange={this.handleChange}
+                value={username}
                 type="text"
               />
               <Form.Input
-                fluid name="email"
+                fluid
+                name="email"
                 icon="mail"
                 iconPosition="left"
                 placeholder="Email Address"
                 onChange={this.handleChange}
+                value={email}
                 type="email"
               />
               <Form.Input
-                fluid name="password"
+                fluid
+                name="password"
                 icon="lock"
                 iconPosition="left"
                 placeholder="Password"
                 onChange={this.handleChange}
+                value={password}
                 type="password"
               />
               <Form.Input
-                fluid name="password"
+                fluid
+                name="passwordConfirmation"
                 icon="lock"
                 iconPosition="left"
                 placeholder="Password confirmation"
                 onChange={this.handleChange}
+                value={passwordConfirmation}
                 type="password"
               />
               <Button color="orange" fluid size="large">Submit</Button>
@@ -56,8 +74,21 @@ class Register extends React.Component {
     );
   }
 
-  handleChange = () => {
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(createdUser => {
+        console.log(createdUser);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 }
 
